6705d82b83eff0bfd0b5a7fb344539011c9cd46f
Mark Fynes <tfynes@vmware.com>
2024-02-16 10:19:49 -0500

    .

--- src/main/java/com/fynesy/lt/LtApplication.java
+++ src/main/java/com/fynesy/lt/LtApplication.java
@@ -1,13 +1,24 @@
 package com.fynesy.lt;
 
+import javax.swing.text.Style;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LtApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	String index() {
+		String s = "hi\n";
+		System.out.println(s);
+		return s;
+	}
 }
